2d3c8292f9fb3b2bf42951653b59073bb0fab2a4
Vagner Santos <santos.vagnerferreira@gmail.com>
2019-03-09 00:35:05 -0300

    chore:(environment) Configs dev environment

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export default {
+  works() {
+    return true;
+  },
+};
---
+++ tests/index.test.js
@@ -0,0 +1,7 @@
+import fc from '../src/index.js';
+
+describe('Some test', () => {
+  test('works', () => {
+    expect(fc.works()).toBeTruthy();
+  });
+});
